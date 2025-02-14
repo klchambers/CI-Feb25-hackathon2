e955d7a21314b56b7d8479d2b88d80324485c14e
Hannah Olbrich <hannaholbrich@gmail.com>
2025-02-14 16:05:53 +0000

    Re-migrate profile models, and amend url on index page for events

--- user_profile/models.py
+++ user_profile/models.py
@@ -6,5 +6,5 @@ class ProfileCategories(models.Model):
     date = models.DateTimeField(max_length = 100)
     email = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
-    age = models.PositiveIntegerField(max_length=3)
+    age = models.PositiveIntegerField(blank=False)
     bio = models.TextField(max_length=500)
