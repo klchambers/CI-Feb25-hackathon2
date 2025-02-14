fd3abe95f7942a026c0dff7b3e463bb959371ced
Hannah Olbrich <hannaholbrich@gmail.com>
2025-02-14 15:31:08 +0000

    Add models to user profile page

--- user_profile/models.py
+++ user_profile/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class ProfileCategories(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateTimeField(max_length = 100)
+    email = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    age = models.PositiveIntegerField(max_length=3)
+    bio = models.TextField(max_length=500)
