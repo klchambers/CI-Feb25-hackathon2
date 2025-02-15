f0b531db83bed6d5f42c651651748e5e2dae07ec
Hannah Olbrich <hannaholbrich@gmail.com>
2025-02-15 12:44:25 +0000

    Slight edits to settings.py

--- dating_event_app/settings.py
+++ dating_event_app/settings.py
@@ -13,9 +13,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
-if os.path.isfile('env.py'):
-    import env
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
 
 # Added as temporary workaround of the: 
 # AttributeError: 'BlankChoiceIterator' object has no attribute '__len__' "
@@ -34,10 +35,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+
+DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
     'dating-events-app-512687071453.herokuapp.com',
