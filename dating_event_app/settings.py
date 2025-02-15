c40ba8f09fc30d6b3d1c502ce1338dba7c8b7e6e
Hannah Olbrich <hannaholbrich@gmail.com>
2025-02-15 12:51:14 +0000

    Change debug back to true

--- dating_event_app/settings.py
+++ dating_event_app/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = 'DEV' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'dating-events-app-512687071453.herokuapp.com',
