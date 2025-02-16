ec0c58bb0667e0e61e2774ac08601019c2d10d1f
Lochlann O'Higgins <164012429+Lochy2000@users.noreply.github.com>
2025-02-16 22:34:26 +0000

    added secure to cloudinary in settings.py

--- dating_event_app/settings.py
+++ dating_event_app/settings.py
@@ -253,7 +253,8 @@ SITE_ID = 1
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': 'your-cloud-name',
     'API_KEY': 'your-api-key',
-    'API_SECRET': 'your-api-secret'
+    'API_SECRET': 'your-api-secret',
+    'SECURE': True
 }
 
 tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
