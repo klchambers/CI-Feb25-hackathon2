b1c25be18f1fd8726588bbf4d2b036e1c87d606c
Kieran Chambers <kieran.chambers@icloud.com>
2025-02-16 13:41:07 +0000

    Feat: Create FAQ model and form

--- faq/models.py
+++ faq/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Faq(models.Model):
+    question = models.CharField(max_length=255)
+    answer = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.question
